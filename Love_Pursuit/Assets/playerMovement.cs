c506cc2ce27ce044aa542ffdc2c561bef8a7141e
warnerej <123595568+warnerej@users.noreply.github.com>
2023-02-25 17:48:07 -0500

    Connecting Rigidbody and Collider

---
+++ Love_Pursuit/Assets/playerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D myRigidBody;
+    public BoxCollider2D myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
