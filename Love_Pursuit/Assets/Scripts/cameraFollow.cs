f686f52a5ac1590921c5cb0a5a040f9e4e346eda
warnerej <123595568+warnerej@users.noreply.github.com>
2023-02-25 21:13:44 -0500

    Update cameraFollow.cs

--- Love_Pursuit/Assets/Scripts/cameraFollow.cs
+++ Love_Pursuit/Assets/Scripts/cameraFollow.cs
@@ -6,13 +6,15 @@ public class cameraFollow : MonoBehaviour
     // Tells the code that the player is the desired target to follow
     public Transform target;
 
-    public float smoothSpeed = 0.5f;
+    public float smoothSpeed = 0.125f;
     public Vector3 offset; // Values to offset the camera from the player by
 
     // Keeps the camera from competing for updates with the player
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        transform.position = target.position + offset;
+        Vector3 desiredPosition = target.position + offset; // Makes the position of the camera 
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // Lets the camera have a mix of where we are and where we want to be when following
+        transform.position = smoothedPosition;
     }
 
 
